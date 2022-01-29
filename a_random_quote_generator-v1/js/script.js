78af09efc4039748750f2145d8d8810074cf1f71
Aa-thomas <Aartho95@gmail.com>
2022-01-28 19:15:29 -0500

    created an array of quotes

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,7 +10,62 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: 'This is quote 1',
+    source: 'source1',
+    citation:'cit1',
+    year:'year1'
+  },
+   {
+    quote:'this is quote 2',
+    source: 'source2',
+    citation: 'cit2',
+    year:'year2'
+  },
+   {
+    quote:'this is quote 3',
+    source:'source3',
+    citation:'cit3',
+    year:'year3'
+  },
+   {
+    quote:'this is quote 4',
+    source:'source4',
+    citation:'cit4',
+    year:'year4'
+  }, {
+    quote:'this is quote 5',
+    source:'source5',
+    citation:'cit5',
+    year:'year5'
+  }, {
+    quote:'this is quote 6',
+    source:'source6',
+    citation:'cit6',
+    year:'year 6'
+  }, {
+    quote:'this is quote 7',
+    source:'source7',
+    citation:'cit7',
+    year:'year7'
+  }, {
+    quote:'this is quote 8',
+    source:'source8',
+    citation:'cit8',
+    year:'year8'
+  }, {
+    quote:'this is quote 9',
+    source:'source9',
+    citation:'cit9',
+    year:'year9'
+  }, {
+    quote:'this is quote 10',
+    source:'source10',
+    citation:'cit10',
+    year:'year10'
+  },
+];
 
 
 /***
